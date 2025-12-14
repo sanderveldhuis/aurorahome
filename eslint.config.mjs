1772ac0dd2de37e5b7f075d6774dd4367b746f3f
sanderveldhuis <mail@sanderveldhuis.nl>
2025-12-14 20:43:19 +0100

    Prepared frontend development environment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import eslint from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -17,7 +19,25 @@ export default tseslint.config(
       globals: globals.node,
       parserOptions: {
         tsconfigRootDir: import.meta.dirname,
-        project: ['./backend/workers/tsconfig-eslint.json']
+        project: ['./backend/workers/tsconfig.json', './backend/workers/tsconfig-eslint.json']
+      }
+    }
+  },
+  {
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite
+    ],
+    files: ['frontend/**'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        project: ['./frontend/tsconfig.node.json', './frontend/tsconfig.app.json']
       }
     }
   }
