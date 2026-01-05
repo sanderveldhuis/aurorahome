c6709d132610aa23557b1e22ca971dc14a8ea679
sanderveldhuis <mail@sanderveldhuis.nl>
2026-01-05 22:11:11 +0100

    Fixing basic pages and layout

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,23 +1,50 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2025 Sander Veldhuis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import { useState } from 'react';
 import './app.css';
-
-/* Import Bootstrap's plugins to enable functionality */
-import { Dropdown } from 'bootstrap'; // eslint-disable-line @typescript-eslint/no-unused-vars
+import Dashboard from './dashboard.tsx';
+import Navbar from './navbar.tsx';
+import Profile from './profile.tsx';
+import Settings from './settings.tsx';
+import Sidebar from './sidebar.tsx';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [component, setComponent] = useState('Dashboard');
 
   return (
     <>
-      <h1>AuroraHome</h1>
-      <div className='card'>
-        <button
-          onClick={() => {
-            setCount(count => count + 1);
-          }}
-        >
-          count is {count}
-        </button>
+      <Navbar setComponent={setComponent}></Navbar>
+      <div className='container-fluid'>
+        <div className='container-row row'>
+          <Sidebar setComponent={setComponent}></Sidebar>
+          <main className='container-col col overflow-y-auto'>
+            <Dashboard component={component}></Dashboard>
+            <Settings component={component}></Settings>
+            <Profile component={component}></Profile>
+          </main>
+        </div>
       </div>
     </>
   );
