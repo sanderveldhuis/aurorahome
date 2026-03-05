01b7a7e407f806b6ab6107a04c0017671f09f981
sanderveldhuis <mail@sanderveldhuis.nl>
2026-03-05 12:24:16 +0100

    Adapt to newest GlideLite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import react from '@vitejs/plugin-react';
+import { glConfig } from 'glidelite/vite';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  ...glConfig,
   plugins: [react()],
   root: path.resolve(__dirname, 'src'),
   publicDir: path.resolve(__dirname, 'public'),
